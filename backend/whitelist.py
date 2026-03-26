00e4ec5a87d3c5344b01637451ae8fc7f09f94bd
Stefan Miesch <hello@miesch.com>
2026-03-26 15:08:36 +0100

    Create whitelist.py

---
+++ backend/whitelist.py
@@ -0,0 +1,26 @@
+# whitelist.py
+# Zentrale Whitelist für alle Checks
+# Ergänzungen hier – keine Code-Änderungen in den Check-Dateien nötig
+
+# Überschriften die nicht als Duplikat gemeldet werden
+HEADING_DUPLICATES_WHITELIST = [
+    "angebot",
+    "angebote", 
+    "kontakt",
+    "über uns",
+    "news",
+    "aktuelles",
+    "home",
+    "startseite",
+    "impressum",
+    "datenschutz",
+    "links",
+    "downloads",
+    "publikationen",
+    "team",
+    "partner",
+]
+
+# Später erweiterbar z.B.:
+# ALT_ATTRIBUTE_WHITELIST = [...]
+# KEYWORD_STOPWORDS = [...]
